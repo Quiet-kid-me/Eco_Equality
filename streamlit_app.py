64909918374fcc421e166b9a85d243f549720213
Quiet Kid <nourhanmm3030@gmail.com>
2024-10-06 03:11:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import pandas as pd
 st.title('Eco_Equality')
 st.info('This app shows the number of women in reproductive age having anemia.')
 
-df = pd.read_csv('https://raw.githubusercontent.com/Quiet-kid-me/Eco_Equality/refs/heads/master/UNdata_Export_20241005_161251297.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/Quiet-kid-me/women_anemia/refs/heads/main/UNdata_Export_20241005_161251297.csv')
