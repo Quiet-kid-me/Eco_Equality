ed55f5efdbda1437064562edd8a5e4e5023f2afe
Quiet Kid <nourhanmm3030@gmail.com>
2024-10-06 10:08:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,17 +6,5 @@ with st.expander('Data'):
   st.write('Raw data')
   df = pd.read_csv('https://raw.githubusercontent.com/Quiet-kid-me/women_anemia/refs/heads/main/data.csv')
   st.dataframe(df)
-# Coordinates for mapping (latitude, longitude)
-coords = {
-    "Afghanistan": [33.93911, 67.709953],  # Approximate center of Afghanistan
-    "Africa": [1.2921, 36.8219],  # Approximate center of Africa (East Africa)
-    "Nepal": [28.3949, 84.1240]  # Approximate center of Nepal
-}
-
-# Extracting latitude and longitude
-df['Latitude'] = df['Country or Area'].map(lambda x: coords[x][0])
-df['Longitude'] = df['Country or Area'].map(lambda x: coords[x][1])
-
-# Create a map
-st.title("Country Data Map")
-st.map(df[['Latitude', 'Longitude', 'Value']])
+  with st.expander('Data visualization')
+  st.scatter_chart(data=df, x='Unit', y='Value', color='Country or Area')
