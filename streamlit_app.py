278e51ff4362bacd5f8189b9b9dc8a478e47abe7
Quiet Kid <nourhanmm3030@gmail.com>
2024-10-06 03:18:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 st.title('Eco_Equality')
 st.info('This app shows the number of women in reproductive age having anemia.')
-with st.expander('Data')
+with st.expander('Data'):
   st.write('Raw data')
   df = pd.read_csv('https://raw.githubusercontent.com/Quiet-kid-me/women_anemia/main/UNdata_Export_20241005_161251297.csv')
   st.dataframe(df)
