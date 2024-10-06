c762213bfc945e1d5b303cdbb99808880bc64239
Quiet Kid <nourhanmm3030@gmail.com>
2024-10-06 03:04:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
 st.title('Eco_Equality')
-
-st.write('Hello world!')
+st.info('This app shows the number of women in reproductive age having anemia.')
+df = pd.read_csv('https://github.com/Quiet-kid-me/women_anemia/blob/main/UNdata_Export_20241005_161251297.csv')
